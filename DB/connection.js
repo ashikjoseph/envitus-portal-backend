6d2439b8f2e288d090ac096c00460f90a6318048
FIRST_NAME LAST_NAME <ashikjoseph17@gmail.com>
2024-11-12 11:37:11 +0530

    changes added

--- DB/connection.js
+++ DB/connection.js
@@ -1,28 +1,12 @@
-const mongoose = require('mongoose');
 
-// Get the connection string from environment variables
-const connectionString = process.env.DATABASE;
+const mongoose = require('mongoose')
 
-// Check if the connection string is missing
-if (!connectionString) {
-    console.error('Error: MongoDB connection string is missing!');
-    process.exit(1); // Exit the application with an error code
-}
 
-// Log the connection string for debugging purposes
-console.log('MongoDB URI:', connectionString);
+const connectionString = process.env.DATABASE
 
-// Set connection options
-const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-};
 
-// Try to connect to MongoDB
-mongoose.connect(connectionString, options)
-    .then(() => {
-        console.log("MongoDB connected successfully!!!");
-    })
-    .catch((err) => {
-        console.error(`MongoDB connection failed due to ${err}`);
-    });
+mongoose.connect(connectionString).then((res)=>{
+    console.log("MongoDB connected successfully!!!")
+}).catch((err)=>{
+    console.log(`MongoDB connect failed due to ${err}`)
+})
